32a97cb04b401a4ddb3f75672e68336adfb206bd
Doris <general.doris@gmail.com>
2019-03-02 22:15:28 +0000

    solution2.py

---
+++ solution2.py
@@ -0,0 +1,13 @@
+x = str(input ("Please enter today's day:"))
+
+l = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ]
+
+a = "Tuesday" or "Thursday"
+
+if x == a:
+    print ("Yes, today begins with 'T'")
+
+else:
+    print ("No, today does not begin with 'T'" )
+
+
